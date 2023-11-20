3f8c0e7bb071610d2d3d2cdd136d20c13b378094
Hey0140 <yeaaen40@gmail.com>
2023-11-20 16:27:06 +0900

    feat: 펫관리 달력페이지

---
+++ app/src/main/java/com/example/myjavaapplication/MonthWalkActivity.java
@@ -0,0 +1,14 @@
+package com.example.myjavaapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MonthWalkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.monthwalk_page);
+    }
+}
